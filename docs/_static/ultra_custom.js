3fcf8769f8b4d01b181ac71b3eaa008b657e2ef9
Kolya Bezgin <bezginn@edi-n.com>
2019-08-02 13:22:31 +0300

    privet

---
+++ docs/_static/ultra_custom.js
@@ -0,0 +1,12 @@
+// Jivosite Chat
+(function () {
+    d = document;
+    s = d.createElement("script");
+    s.src = "//code.jivosite.com/widget.js";
+    s.async = 1;
+    s.setAttribute("jv-id", "iEUmonxahY");
+    d.getElementsByTagName("head")[0].appendChild(s);
+})
+();
+
+
