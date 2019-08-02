d20afa9d9496bf68587be1aa164278eeb80ba52c
Kolya Bezgin <bezginn@edi-n.com>
2019-08-02 16:47:24 +0300

    ali ruli

--- docs/_static/OneSignalSDKWorker.js
+++ docs/_static/OneSignalSDKWorker.js
@@ -1,1 +1,13 @@
-importScripts('https://js.pusher.com/5.0/pusher.worker.min.js');
+window.OneSignal = window.OneSignal || [];
+var initOptions = {
+  appId: "7b5d5a38-eca8-4bfd-a89b-623c20fbd404",
+  subdomainName: 'onesignal-demo',
+  autoRegister: true,
+  notifyButton: {
+    enable: true
+  }
+};
+
+OneSignal.push(function() {
+  OneSignal.init(initOptions);
+});
