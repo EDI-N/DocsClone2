7a3dc827f063ed9a67eaccc15c2439acfc3b2fe8
Kolya Bezgin <bezginn@edi-n.com>
2019-10-18 15:47:02 +0300

    404 neded

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.contentui',
+    'notfound.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
